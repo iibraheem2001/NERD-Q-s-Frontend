805592a407824a45a8a479eb941238367e2fec70
Cedric Vallieu <cvallieu94@gmail.com>
2022-02-08 13:03:12 -0800

    Prototyped session config dropdown from material ui

--- src/layouts/dashboard/components/TraviaOverview/index.js
+++ src/layouts/dashboard/components/TraviaOverview/index.js
@@ -1,9 +1,10 @@
 // @mui material components
 import Card from "@mui/material/Card";
 
-// NERDQ React components
+// NERD Q's React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
+import ComboBox from "./AutoComplete/Dropdown";
 
 function TraviaOverview() {
   return (
@@ -12,6 +13,12 @@ function TraviaOverview() {
         <MDTypography variant="h6" fontWeight="medium">
           Travia overview
         </MDTypography>
+        <MDTypography variant="h6" fontWeight="medium">
+          Click to start session
+        </MDTypography>
+        <MDBox p={2}>
+          <ComboBox />
+        </MDBox>
       </MDBox>
     </Card>
   );
